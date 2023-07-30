8b7bb9d3ebedf71d2d651d22024d5e34f68d2da6
Roger Hyam <rhyam@rbge.org.uk>
2023-07-30 14:34:30 +0100

    fix path

--- scripts/kew_code_sync_from_staging.sh
+++ scripts/kew_code_sync_from_staging.sh
@@ -3,4 +3,4 @@
 # It will be used when it, and the rest of the code, doesn't exist.
 # it also needs to be in the same place as the other sync_from and sync_to scripts
 rsync -Pav -e "ssh -i ~/.ssh/wfo-aws-03.pem" --delete --exclude 'data' --exclude '.git'  wfo@wfo-staging.rbge.info:/var/wfo-list/kew_sync /var/wfo-list/
-mkdir /var/wfo-list/data
+mkdir /var/wfo-list/kew_sync/data
